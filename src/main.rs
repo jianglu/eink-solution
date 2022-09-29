5041d9b704ba65d2fd70170d6c54c1ea715307cb
Jiang Lu <jianglu@ensurebit.com>
2022-09-29 09:08:46 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,79 @@
+//
+// Copyright (C) Lenovo ThinkBook Gen4 Project.
+//
+// This program is protected under international and China copyright laws as
+// an unpublished work. This program is confidential and proprietary to the
+// copyright owners. Reproduction or disclosure, in whole or in part, or the
+// production of derivative works therefrom without the express permission of
+// the copyright owners is prohibited.
+//
+// All rights reserved.
+//
+
+#[macro_use]
+extern crate windows_service;
+
+use std::{ffi::OsString, time::Duration};
+use windows_service::{
+    service::{
+        ServiceControl, ServiceControlAccept, ServiceExitCode, ServiceState, ServiceStatus,
+        ServiceType,
+    },
+    service_control_handler::{self, ServiceControlHandlerResult},
+    service_dispatcher, Result,
+};
+
+define_windows_service!(ffi_service_main, my_service_main);
+
+fn my_service_main(arguments: Vec<OsString>) {
+    // The entry point where execution will start on a background thread after a call to
+    // `service_dispatcher::start` from `main`.
+    for arg in arguments {
+        println!("Arg: {:?}", &arg);
+    }
+
+    // 处理服务控制事件
+    let event_handler = move |control_event| -> ServiceControlHandlerResult {
+        match control_event {
+            ServiceControl::Stop | ServiceControl::Interrogate => {
+                ServiceControlHandlerResult::NoError
+            }
+            _ => {
+                println!("Other Control Event Not Implemented");
+                ServiceControlHandlerResult::NotImplemented
+            }
+        }
+    };
+
+    // Register system service event handler
+    let status_handle = service_control_handler::register("eink_service", event_handler).unwrap();
+
+    let next_status = ServiceStatus {
+        // Should match the one from system service registry
+        service_type: ServiceType::OWN_PROCESS,
+        // The new state
+        current_state: ServiceState::Running,
+        // Accept stop events when running
+        controls_accepted: ServiceControlAccept::STOP,
+        // Used to report an error when starting or stopping only, otherwise must be zero
+        exit_code: ServiceExitCode::Win32(0),
+        // Only used for pending states, otherwise must be zero
+        checkpoint: 0,
+        // Only used for pending states, otherwise must be zero
+        wait_hint: Duration::default(),
+        // Unused for setting status
+        process_id: None,
+    };
+
+    // Tell the system that the service is running now
+    status_handle.set_service_status(next_status)?;
+
+    // Do some work
+}
+
+fn main() -> Result<()> {
+    // Register generated `ffi_service_main` with the system and start the service, blocking
+    // this thread until the service is stopped.
+    service_dispatcher::start("myservice", ffi_service_main)?;
+    Ok(())
+}
