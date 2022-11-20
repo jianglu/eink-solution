09d9101b0fac6fa5090dd510282f4f23ec9b38e5
Jiang Lu <jianglu@ensurebit.com>
2022-11-20 18:30:44 +0800

    eink-logger 增加 panic output 和 backtrace dumping 功能

--- crates/eink-logger/src/lib.rs
+++ crates/eink-logger/src/lib.rs
@@ -148,5 +148,20 @@ pub fn init_with_level(_level: log::Level) -> anyhow::Result<()> {
     Ok(())
 }
 
+/// 初始化 Panic 的输出为 OutputDebugString
+pub fn init_panic_output() {
+    std::panic::set_hook(Box::new(|info| {
+        log::error!("PANIC: {:?}", info);
+        let backtrace = std::backtrace::Backtrace::force_capture();
+        log::error!("BACKTRACE:\n{}", backtrace.to_string());
+    }));
+}
+
 #[test]
-fn test_get_eink_logging_dir() {}
+fn test_backtrace_split() {
+    let re = regex::Regex::new(r"(\}, \{)|(\[\{)").unwrap();
+    let text = r#"Backtrace [{ fn: "std::backtrace_rs::backtrace::dbghelp::trace", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\..\..\backtrace\src\backtrace\dbghelp.rs", line: 98 }, { fn: "std::backtrace_rs::backtrace::trace_unsynchronized", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\..\..\backtrace\src\backtrace\mod.rs", line: 66 }, { fn: "std::backtrace::Backtrace::create", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\backtrace.rs", line: 332 }, { fn: "std::backtrace::Backtrace::force_capture", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\backtrace.rs", line: 314 }, { fn: "eink_service::init_panic_output::closure$0", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-service\src\main.rs", line: 53 }, { fn: "alloc::boxed::impl$47::call", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\alloc\src\boxed.rs", line: 2032 }, { fn: "std::panicking::rust_panic_with_hook", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\panicking.rs", line: 692 }, { fn: "std::panicking::begin_panic_handler::closure$0", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\panicking.rs", line: 579 }, { fn: "std::sys_common::backtrace::__rust_end_short_backtrace<std::panicking::begin_panic_handler::closure_env$0,never$>", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\sys_common\backtrace.rs", line: 137 }, { fn: "std::panicking::begin_panic_handler", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\std\src\panicking.rs", line: 575 }, { fn: "core::panicking::panic_fmt", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b/library\core\src\panicking.rs", line: 65 }, { fn: "core::panicking::panic_display<windows_dll::Error<enum2$<eink_itetcon::itetcon::ITEGetDriveNo> > >", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b\library\core\src\panicking.rs", line: 138 }, { fn: "eink_itetcon::itetcon::ITEGetDriveNo::closure$0", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-itetcon\src\itetcon.rs", line: 60 }, { fn: "enum2$<core::result::Result<u32 (*)(ref_mut$<u8>),windows_dll::Error<enum2$<eink_itetcon::itetcon::ITEGetDriveNo> > > >::unwrap_or_else<u32 (*)(ref_mut$<u8>),windows_dll::Error<enum2$<eink_itetcon::itetcon::ITEGetDriveNo> >,eink_itetcon::itetcon::ITEGetDr", file: "/rustc/c5d82ed7a4ad94a538bb87e5016e7d5ce0bd434b\library\core\src\result.rs", line: 1504 }, { fn: "eink_itetcon::itetcon::ITEGetDriveNo", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-itetcon\src\itetcon.rs", line: 60 }, { fn: "eink_itetcon::itetcon_device::IteTconDevice::open", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-itetcon\src\itetcon_device.rs", line: 60 }, { fn: "eink_service::tcon_service::TconService::start", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-service\src\tcon_service.rs", line: 72 }, { fn: "eink_service::service_main::run_service", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-service\src\service_main.rs", line: 139 }, { fn: "eink_service::service_main", file: "C:\Users\JiangLu\lenovo-thinkbook-gen4\eink-solution\crates\eink-service\src\main.rs", line: 93 }, { fn: "eink_service::ffi_service_main", file: "C:\Users\JiangLu\.cargo\registry\src\mirrors.tuna.tsinghua.edu.cn-df7c3c540f42cdbd\windows-service-0.5.0\src\service_dispatcher.rs", line: 53 }, { fn: "QueryServiceConfig2W" }, { fn: "BaseThreadInitThunk" }, { fn: "RtlUserThreadStart" }]"#;
+    for line in re.split(text) {
+        println!("{}", line.trim());
+    }
+}
