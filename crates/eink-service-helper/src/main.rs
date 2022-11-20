01a3fcf84799afb814132a577da1c7fc1034a60a
Jiang Lu <jianglu@ensurebit.com>
2022-11-20 19:22:07 +0800

    清理 eink-service-helper 无用的 imports

--- crates/eink-service-helper/src/main.rs
+++ crates/eink-service-helper/src/main.rs
@@ -30,55 +30,24 @@ mod win_utils;
 mod window;
 mod wmi_service;
 
-use std::ffi::c_void;
-use std::sync::atomic::{AtomicBool, AtomicI32, AtomicIsize, AtomicU32, Ordering};
+use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::Arc;
 
-use always_on_top::{AlwaysOnTop, ALWAYS_ON_TOP};
-use anyhow::bail;
-use log::info;
+use always_on_top::ALWAYS_ON_TOP;
 use ls_note_starter::LockScreenNoteManager;
-use mag_win::MagWindow;
 use ntapi::winapi::um::winnt;
-use parking_lot::{Mutex, RwLock};
-use settings::SETTINGS;
+use parking_lot::Mutex;
 use structopt::StructOpt;
-use tokio::runtime::Runtime;
 use topmost::TOPMOST_MANAGER;
-use utils::get_current_exe_dir;
-use win_utils::run_as_admin;
-use windows::core::*;
 use windows::Win32::Foundation::*;
-use windows::Win32::Graphics::Gdi::ValidateRect;
-use windows::Win32::System::Console::GetConsoleWindow;
-use windows::Win32::System::LibraryLoader::GetModuleHandleA;
-use windows::Win32::System::Threading::{
-    CreateThreadpoolTimer, SetThreadpoolTimer, TP_CALLBACK_INSTANCE, TP_TIMER,
-};
-use windows::Win32::UI::HiDpi::{
-    SetProcessDpiAwarenessContext, DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2,
-};
-use windows::Win32::UI::Input::KeyboardAndMouse::{
-    keybd_event, KEYBD_EVENT_FLAGS, KEYEVENTF_KEYUP,
-};
-use windows::Win32::UI::Magnification::{
-    MagGetWindowFilterList, MagInitialize, MagSetColorEffect, MagSetWindowFilterList,
-    MagUninitialize, MW_FILTERMODE, MW_FILTERMODE_EXCLUDE, MW_FILTERMODE_INCLUDE,
-};
-use windows::Win32::UI::WindowsAndMessaging::*;
-use windows_hotkeys::keys::winapi_keycodes::VK_LWIN;
 use windows_hotkeys::keys::{ModKey, VKey};
 use windows_hotkeys::HotkeyManager;
-use wineventhook::raw_event::{OBJECT_CREATE, SYSTEM_FOREGROUND};
-use wineventhook::{AccessibleObjectId, EventFilter, WindowEventHook};
 use winnt::KEY_ALL_ACCESS;
 use winreg::enums::HKEY_LOCAL_MACHINE;
 use winreg::RegKey;
 use wmi_service::WMI_SERVICE;
 
 use crate::mode_manager::MODE_MANAGER;
-use crate::specialized::set_monitor_specialized;
-use crate::window::{enumerate_all_windows, enumerate_capturable_windows};
 
 type AnyResult<T> = anyhow::Result<T>;
 
@@ -96,97 +65,6 @@ struct Opt {
     #[structopt(short = "c", long = "config-file")]
     _config_file: Option<String>,
 }
-
-// /// 切换到 EINK Launcher 模式
-// fn switch_to_eink_launcher_mode() {
-//     log::info!("switch_to_eink_launcher_mode");
-
-//     if let Ok(eink_monitor_id) = SETTINGS.read().get_string("eink_monitor_id") {
-//         if eink_monitor_id.len() > 8 {
-//             set_monitor_specialized(&eink_monitor_id, false).unwrap();
-//         }
-//         if let Ok(oled_monitor_id) = SETTINGS.read().get_string("oled_monitor_id") {
-//             if oled_monitor_id.len() > 8 {
-//                 set_monitor_specialized(&oled_monitor_id, true).unwrap();
-
-//                 // Sleep 200ms 等待 TCON 稳定后才重置
-//                 std::thread::sleep(std::time::Duration::from_millis(200));
-
-//                 // 切换到eink时，要软件启动一下
-//                 tcon_api::eink_software_reset_tcon();
-
-//                 // 置顶 Launcher
-//                 find_launcher_and_set_topmost();
-
-//                 // 置顶悬浮球
-//                 find_floating_button_and_set_topmost();
-
-//                 // 将当前模式保存到注册表
-//                 save_display_mode_to_registry("EINK");
-
-//                 // 使用 WMI 接口通知主板, 1 : OLED is Working
-//                 WMI_SERVICE.lock().set_display_working_status(2);
-
-//                 WMI_SERVICE.lock().get_display_working_status();
-
-//                 IS_OLED.store(false, Ordering::Relaxed);
-
-//                 // 重置 DPI 作为保护性操作，可以在非关键上下文中运行
-//                 // 将 EINK 屏幕的 DPI 设置为 200，成功后再次重新尝试置顶 Launcher
-//                 std::thread::spawn(move || {
-//                     if let Err(err) = monitor::set_dpi_by_stable_monitor_id(&eink_monitor_id, 200) {
-//                         log::error!("Cannot reset eink dpi to 200: err: {err}");
-//                     } else {
-//                         find_launcher_and_set_topmost();
-//                     }
-
-//                     // OLED 桌面模式采用 Hybrid Hybrid 模式
-
-//                     // GI_MIPI_BROWSER = 0x02;
-//                     // GI_MIPI_HYBRID = 0xF0;
-//                     tcon_api::eink_set_mipi_mode(0xF0);
-//                 });
-//             }
-//         }
-//     }
-// }
-
-// // 切换搭配 OLED Windows 桌面模式
-// fn switch_to_oled_windows_desktop_mode() {
-//     log::info!("switch_to_oled_windows_desktop_mode");
-
-//     if let Ok(oled_monitor_id) = SETTINGS.read().get_string("oled_monitor_id") {
-//         set_monitor_specialized(&oled_monitor_id, false).unwrap();
-
-//         if let Ok(eink_monitor_id) = SETTINGS.read().get_string("eink_monitor_id") {
-//             set_monitor_specialized(&eink_monitor_id, true).unwrap();
-
-//             // OLED 桌面模式采用 Hybrid Browser 模式
-
-//             // GI_MIPI_BROWSER = 0x02;
-//             // GI_MIPI_HYBRID = 0xF0;
-//             //tcon_api::eink_set_mipi_mode(0x02);
-
-//             // 最小化 Launcher
-//             find_launcher_and_set_hidden();
-
-//             // 清除当前置顶的窗口
-//             TOPMOST_MANAGER.lock().clear_current_topmost_window();
-
-//             // 将当前模式保存到注册表
-//             save_display_mode_to_registry("OLED");
-
-//             // 使用 WMI 接口通知主板, 2 : E-ink is Working
-//             WMI_SERVICE.lock().set_display_working_status(1);
-
-//             WMI_SERVICE.lock().get_display_working_status();
-
-//             IS_OLED.store(true, Ordering::Relaxed);
-
-//             tcon_api::eink_show_shutdown_cover();
-//         }
-//     }
-// }
 
 /// 将当前显示模式保存到注册表
 pub fn save_display_mode_to_registry(mode: &str) {
@@ -261,12 +139,12 @@ fn main() -> AnyResult<()> {
     TOPMOST_MANAGER.lock().start().unwrap();
 
     WMI_SERVICE.lock().on_lid_event(|evt| {
-        info!("Received LidEvent: {:?}", evt);
+        log::info!("Received LidEvent: {:?}", evt);
     });
 
     // Cases: 4 -> 11 -> 3
     WMI_SERVICE.lock().on_mode_switch_event(|mode| {
-        info!("Received OnModeSwitchEvent: {:?}", mode);
+        log::info!("Received OnModeSwitchEvent: {:?}", mode);
 
         let last_mode = LAST_MODE.load(Ordering::Relaxed);
 
@@ -296,7 +174,7 @@ fn main() -> AnyResult<()> {
                 MODE_MANAGER.lock().request_to_eink_launcher_mode();
             }
             _ => {
-                info!("Unused mode : {mode}")
+                log::info!("Unused mode : {mode}")
             }
         }
 
@@ -372,7 +250,7 @@ fn main() -> AnyResult<()> {
 
     // CTRL-Shift-F13 进入 EINK
     match hkm.register(VKey::F13, &[ModKey::Ctrl, ModKey::Shift], move || {
-        info!("Clicked: CTRL-Shift-F13");
+        log::info!("Clicked: CTRL-Shift-F13");
         MODE_MANAGER.lock().request_switch_eink_oled_display();
     }) {
         Ok(_) => (), // ignore
@@ -383,7 +261,7 @@ fn main() -> AnyResult<()> {
 
     // CTRL-Shift-F14
     match hkm.register(VKey::F14, &[ModKey::Ctrl, ModKey::Shift], move || {
-        info!("Clicked: CTRL-Shift-F14")
+        log::info!("Clicked: CTRL-Shift-F14")
     }) {
         Ok(_) => (), // ignore
         Err(err) => {
@@ -393,7 +271,7 @@ fn main() -> AnyResult<()> {
 
     // CTRL-Shift-F15
     match hkm.register(VKey::F15, &[ModKey::Ctrl, ModKey::Shift], move || {
-        info!("Clicked: CTRL-Shift-F15")
+        log::info!("Clicked: CTRL-Shift-F15")
     }) {
         Ok(_) => (), // ignore
         Err(err) => {
@@ -402,7 +280,7 @@ fn main() -> AnyResult<()> {
     }
 
     // 进入 OLED 桌面模式
-    info!("After system-up, switch to oled desktop mode");
+    log::info!("After system-up, switch to oled desktop mode");
     MODE_MANAGER.lock().request_to_oled_windows_desktop_mode();
 
     hkm.event_loop();
